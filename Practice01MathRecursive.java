4d91360647c6905721a9457079f08e6b92238ed2
pbadola76 <pujabadola2698@gmail.com>
2020-01-29 15:48:19 -0800

    Update Practice01MathRecursive.java

--- Practice01MathRecursive.java
+++ Practice01MathRecursive.java
@@ -5,12 +5,6 @@
 public class Practice01MathRecursive implements Practice01Math {
   public int fib(int n) throws Exception
   {
-      // if (n < 1)
-      // {
-      //   return fib(n - 1) + fib(n -2);
-      // } else {
-      //   return n;
-      // }
 
       if (n < 0)
       {
